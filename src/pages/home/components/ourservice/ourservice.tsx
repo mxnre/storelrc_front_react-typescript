a6f429b3166884c42830e2ca268da72312015d57
mxnre <mnteristo@gmail.com>
2020-10-24 04:52:28 +0900

    Updated Header and Fotter and some css

--- src/pages/home/components/ourservice/ourservice.tsx
+++ src/pages/home/components/ourservice/ourservice.tsx
@@ -8,26 +8,25 @@ export const OurService = () => {
       <Row className="home-ourservice-row">
         <Col md={4}>
           <div className="home-ourservice-item">
-            <img src="./assets/svg/svgexport-1.svg" width='100px' height='100px' />
+            <img className="home-ourservice__image" src="./assets/svg/svgexport-1.svg" width='96px' height='96px' />
             <p className="home-ourservice-item-title">Sur Mesure</p>
             <p className="home-ourservice-item-text">Réalisation sur mesure et<br/> adaptation aux contraintes de<br/> votre établissement</p>
           </div>
         </Col>
         <Col md={4}>
           <div className="home-ourservice-item">
-            <img src="./assets/svg/svgexport-2.svg" width='100px' height='100px' />
+            <img src="./assets/svg/svgexport-2.svg" width='96px' height='96px' />
             <p className="home-ourservice-item-title">Livraison rapide</p>
             <p className="home-ourservice-item-text">Livraison rapide de votre<br/> lambrequin lumineux dans<br/>toute la France</p>
           </div>
         </Col>
         <Col md={4}>
           <div className="home-ourservice-item">
-            <img src="./assets/svg/svgexport-3.svg" width='100px' height='100px' />
+            <img className="home-ourservice__image" src="./assets/svg/svgexport-3.svg" width='96px' height='96px' />
             <p className="home-ourservice-item-title">Qualité</p>
             <p className="home-ourservice-item-text">Nos lambrequins lumineux<br/>sont réalisés à partir de<br/>matériaux de haute qualité</p>
           </div>
         </Col>
       </Row>
-
   );
 }
